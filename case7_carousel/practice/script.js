e526e851c9a1e9b8d6fc0462dc53403e34d09c90
Jun Pyo Seo <cooljp95@naver.com>
2022-11-19 21:40:55 +0900

    case9 까지 완성

--- case7_carousel/practice/script.js
+++ case7_carousel/practice/script.js
@@ -1,87 +1,101 @@
-;(function () {
-  'use strict'
+(function () {
+  'use strict';
 
   const get = (target) => {
-    return document.querySelector(target)
-  }
+    return document.querySelector(target);
+  };
 
   class Carousel {
-    constructor(caruoselElement) {
-      this.caruoselElement = caruoselElement
-      this.itemClassName = 'carousel_item'
-      this.items = this.caruoselElement.querySelectorAll('.carousel_item')
-      this.totalItems = this.items.length
-      this.current = 0
+    constructor(carouselElemnet) {
+      this.carouselElemnet = carouselElemnet;
+      this.itemClassName = 'carousel_item';
+      this.items = this.carouselElemnet.querySelectorAll('.carousel_item');
+
+      this.totalItems = this.items.length;
+      this.current = 0;
+      this.isMoving = false;
     }
 
     initCarousel() {
-      this.items[this.totalItems - 1].classList.add('prev')
-      this.items[0].classList.add('active')
-      this.items[1].classList.add('next')
+      this.isMoving = false;
+      this.items[this.totalItems - 1].classList.add('prev');
+      this.items[0].classList.add('active');
+      this.items[1].classList.add('next');
+    }
+
+    disableInteration() {
+      this.isMoving = true;
+      setTimeout(() => {
+        this.isMoving = false;
+      }, 500);
+    }
+
+    setEventListener() {
+      this.prevButton = this.carouselElemnet.querySelector(
+        '.carousel_button--prev'
+      );
+      this.nextButton = this.carouselElemnet.querySelector(
+        '.carousel_button--next'
+      );
+
+      this.prevButton.addEventListener('click', () => {
+        this.moveprev();
+      });
+      this.nextButton.addEventListener('click', () => {
+        this.movenext();
+      });
     }
 
     moveCarouselTo() {
-      let prev = this.current - 1
-      let next = this.current + 1
+      if (this.isMoving) return;
+      this.disableInteration();
+      let prev = this.current - 1;
+      let next = this.current + 1;
 
       if (this.current === 0) {
-        prev = this.totalItems - 1
+        prev = this.totalItems - 1;
       } else if (this.current === this.totalItems - 1) {
-        next = 0
+        next = 0;
       }
 
       for (let i = 0; i < this.totalItems; i++) {
         if (i === this.current) {
-          this.items[i].className = this.itemClassName + ' active'
+          this.items[i].className = this.itemClassName + ' active';
         } else if (i === prev) {
-          this.items[i].className = this.itemClassName + ' prev'
+          this.items[i].className = this.itemClassName + ' prev';
         } else if (i === next) {
-          this.items[i].className = this.itemClassName + ' next'
+          this.items[i].className = this.itemClassName + ' next';
         } else {
-          this.items[i].className = this.itemClassName
+          this.items[i].className = this.itemClassName;
         }
       }
     }
 
-    movePrev() {
-      if (this.current === 0) {
-        this.current = this.totalItems - 1
-      } else {
-        this.current--
-      }
-      this.moveCarouselTo()
-    }
-
-    moveNext() {
+    movenext() {
+      if (this.isMoving) return;
       if (this.current === this.totalItems - 1) {
-        this.current = 0
+        this.current = 0;
       } else {
-        this.current++
+        this.current++;
       }
-      this.moveCarouselTo()
+      this.moveCarouselTo();
     }
 
-    setEventListeners() {
-      this.prevButton = this.caruoselElement.querySelector(
-        '.carousel_button--prev'
-      )
-      this.nextButton = this.caruoselElement.querySelector(
-        '.carousel_button--next'
-      )
-
-      this.prevButton.addEventListener('click', () => {
-        this.movePrev()
-      })
-      this.nextButton.addEventListener('click', () => {
-        this.moveNext()
-      })
+    moveprev() {
+      if (this.isMoving) return;
+      if (this.current === 0) {
+        this.current = this.totalItems - 1;
+      } else {
+        this.current--;
+      }
+      this.moveCarouselTo();
     }
   }
 
   document.addEventListener('DOMContentLoaded', () => {
-    const carouselElement = get('.carousel')
-    const carousel = new Carousel(carouselElement)
-    carousel.initCarousel()
-    carousel.setEventListeners()
-  })
-})()
+    const carouselElemnet = get('.carousel');
+    const carousel = new Carousel(carouselElemnet);
+    carousel.initCarousel();
+    carousel.setEventListener();
+  });
+})();
--- case9_todo_list_recommend/practice/script.js
+++ case9_todo_list_recommend/practice/script.js
@@ -1,75 +1,76 @@
-;(function () {
-  'use strict'
+(function () {
+  'use strict';
 
   const get = (target) => {
-    return document.querySelector(target)
-  }
+    return document.querySelector(target);
+  };
   const getAll = (target) => {
-    return document.querySelectorAll(target)
-  }
+    return document.querySelectorAll(target);
+  };
 
-  const $todos = get('.todos')
-  const $form = get('.todo_form')
-  const $todoInput = get('.todo_input')
-  const $pagination = get('.pagination')
-  const API_URL = `http://localhost:3000/todos`
+  const $todos = get('.todos');
+  const $form = get('.todo_form');
+  const $todoInput = get('.todo_input');
+  const $pagination = get('.pagination');
+  const API_URL = `http://localhost:3000/todos`;
 
-  let currentPage = 1
-  const totalCount = 53
-  const pageCount = 5
-  const limit = 5
+  let currentPage = 1;
+  const totalCount = 53;
+  const pageCount = 5;
+  const limit = 5;
 
   const pagination = () => {
-    let totalPage = Math.ceil(totalCount / limit)
-    let pageGroup = Math.ceil(currentPage / pageCount)
-    let lastNumber = pageGroup * pageCount
+    let totalPage = Math.ceil(totalCount / limit);
+    let pageGroup = Math.ceil(currentPage / pageCount);
+    let lastNumber = pageGroup * pageCount;
     if (lastNumber > totalPage) {
-      lastNumber = totalPage
+      lastNumber = totalPage;
     }
-    let firstNumber = lastNumber - (pageCount - 1)
+    let firstNumber = lastNumber - (pageCount - 1);
 
-    const next = lastNumber + 1
-    const prev = firstNumber - 1
+    const next = lastNumber + 1;
+    const prev = firstNumber - 1;
 
-    let html = ''
+    let html = '';
 
     if (prev > 0) {
-      html += "<button class='prev' data-fn='prev'>이전</button> "
+      html += "<button class='prev' data-fn='prev'>이전</button> ";
     }
 
     for (let i = firstNumber; i <= lastNumber; i++) {
-      html += `<button class="pageNumber" id="page_${i}">${i}</button>`
+      html += `<button class="pageNumber" id="page_${i}">${i}</button>`;
     }
     if (lastNumber < totalPage) {
-      html += `<button class='next' data-fn='next'>다음</button>`
+      html += `<button class='next' data-fn='next'>다음</button>`;
     }
 
-    $pagination.innerHTML = html
-    const $currentPageNumber = get(`.pageNumber#page_${currentPage}`)
-    $currentPageNumber.style.color = '#9dc0e8'
+    $pagination.innerHTML = html;
+    const $currentPageNumber = get(`.pageNumber#page_${currentPage}`);
+    $currentPageNumber.style.color = '#9dc0e8';
 
-    const $currentPageNumbers = getAll(`.pagination button`)
+    const $currentPageNumbers = getAll(`.pagination button`);
     $currentPageNumbers.forEach((button) => {
       button.addEventListener('click', () => {
         if (button.dataset.fn === 'prev') {
-          currentPage = prev
+          currentPage = prev;
         } else if (button.dataset.fn === 'next') {
-          currentPage = next
+          currentPage = next;
         } else {
-          currentPage = button.innerText
+          currentPage = button.innerText;
         }
-        pagination()
-        getTodos()
-      })
-    })
-  }
+        pagination();
+        getTodos();
+      });
+    });
+  };
 
   const createTodoElement = (item) => {
-    const { id, content, completed } = item
-    const isChecked = completed ? 'checked' : ''
-    const $todoItem = document.createElement('div')
-    $todoItem.classList.add('item')
-    $todoItem.dataset.id = id
+    const { id, content, completed, recommended } = item;
+    const isChecked = completed ? 'checked' : '';
+    const isRecommend = recommended ? 'active' : '';
+    const $todoItem = document.createElement('div');
+    $todoItem.classList.add('item');
+    $todoItem.dataset.id = id;
     $todoItem.innerHTML = `
             <div class="content">
               <input
@@ -77,10 +78,14 @@
                 class='todo_checkbox'
                 ${isChecked}
               />
-              <label>${content}</label>
+              <label class="title">${content}</label>
               <input type="text" value="${content}" />
             </div>
             <div class="item_buttons content_buttons">
+              <button class="todo_recommend_button ${isRecommend}">
+                <i class="fa-regular fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+              </button>
               <button class="todo_edit_button">
                 <i class="far fa-edit"></i>
               </button>
@@ -96,35 +101,35 @@
                 <i class="fas fa-times"></i>
               </button>
             </div>
-      `
-    return $todoItem
-  }
+      `;
+    return $todoItem;
+  };
 
   const renderAllTodos = (todos) => {
-    $todos.innerHTML = ''
+    $todos.innerHTML = '';
     todos.forEach((item) => {
-      const todoElement = createTodoElement(item)
-      $todos.appendChild(todoElement)
-    })
-  }
+      const todoElement = createTodoElement(item);
+      $todos.appendChild(todoElement);
+    });
+  };
 
   const getTodos = () => {
     fetch(`${API_URL}?_page=${currentPage}&_limit=${limit}`)
       .then((response) => response.json())
       .then((todos) => {
-        renderAllTodos(todos)
+        renderAllTodos(todos);
       })
-      .catch((error) => console.error(error.message))
-  }
+      .catch((error) => console.error(error.message));
+  };
 
   const addTodo = (e) => {
-    e.preventDefault()
-    const content = $todoInput.value
-    if (!content) return
+    e.preventDefault();
+    const content = $todoInput.value;
+    if (!content) return;
     const todo = {
       content,
       completed: false,
-    }
+    };
     fetch(API_URL, {
       method: 'POST',
       headers: { 'Content-type': 'application/json' },
@@ -133,17 +138,17 @@
       .then((response) => response.json())
       .then(getTodos)
       .then(() => {
-        $todoInput.value = ''
-        $todoInput.focus()
+        $todoInput.value = '';
+        $todoInput.focus();
       })
-      .catch((error) => console.error(error.message))
-  }
+      .catch((error) => console.error(error.message));
+  };
 
   const toggleTodo = (e) => {
-    if (e.target.className !== 'todo_checkbox') return
-    const $item = e.target.closest('.item')
-    const id = $item.dataset.id
-    const completed = e.target.checked
+    if (e.target.className !== 'todo_checkbox') return;
+    const $item = e.target.closest('.item');
+    const id = $item.dataset.id;
+    const completed = e.target.checked;
     fetch(`${API_URL}/${id}`, {
       method: 'PATCH',
       headers: { 'Content-type': 'application/json' },
@@ -151,78 +156,100 @@
     })
       .then((response) => response.json())
       .then(getTodos)
-      .catch((error) => console.error(error.message))
-  }
+      .catch((error) => console.error(error.message));
+  };
 
-  const changeEditMode = (e) => {
-    const $item = e.target.closest('.item')
-    const $label = $item.querySelector('label')
-    const $editInput = $item.querySelector('input[type="text"]')
-    const $contentButtons = $item.querySelector('.content_buttons')
-    const $editButtons = $item.querySelector('.edit_buttons')
-    const value = $editInput.value
-
-    if (e.target.className === 'todo_edit_button') {
-      $label.style.display = 'none'
-      $editInput.style.display = 'block'
-      $contentButtons.style.display = 'none'
-      $editButtons.style.display = 'block'
-      $editInput.focus()
-      $editInput.value = ''
-      $editInput.value = value
-    }
-
-    if (e.target.className === 'todo_edit_cancel_button') {
-      $label.style.display = 'block'
-      $editInput.style.display = 'none'
-      $contentButtons.style.display = 'block'
-      $editButtons.style.display = 'none'
-      $editInput.value = $label.innerText
-    }
-  }
-
-  const editTodo = (e) => {
-    if (e.target.className !== 'todo_edit_confirm_button') return
-    const $item = e.target.closest('.item')
-    const id = $item.dataset.id
-    const $editInput = $item.querySelector('input[type="text"]')
-    const content = $editInput.value
-
+  const recommendTodo = (e) => {
+    if (!e.target.classList.contains('todo_recommend_button')) return;
+    const $item = e.target.closest('.item');
+    const id = $item.dataset.id;
+    const recommended = !e.target.classList.contains('active');
     fetch(`${API_URL}/${id}`, {
       method: 'PATCH',
       headers: { 'Content-type': 'application/json' },
-      body: JSON.stringify({ content }),
+      body: JSON.stringify({ recommended }),
     })
       .then((response) => response.json())
       .then(getTodos)
-      .catch((error) => console.error(error.message))
-  }
+      .catch((error) => console.error(error.message));
+  };
+
+  const changeEditMode = (e) => {
+    const $item = e.target.closest('.item');
+    const $label = $item.querySelector('label');
+    const $editInput = $item.querySelector('input[type="text"]');
+    const $contentButtons = $item.querySelector('.content_buttons');
+    const $editButtons = $item.querySelector('.edit_buttons');
+    const value = $editInput.value;
+
+    if (
+      e.target.className === 'todo_edit_button' ||
+      e.target.className === 'title'
+    ) {
+      $label.style.display = 'none';
+      $editInput.style.display = 'block';
+      $contentButtons.style.display = 'none';
+      $editButtons.style.display = 'block';
+      $editInput.focus();
+      $editInput.value = '';
+      $editInput.value = value;
+    }
+
+    if (e.target.className === 'todo_edit_cancel_button' || e.keyCode === 27) {
+      $label.style.display = 'block';
+      $editInput.style.display = 'none';
+      $contentButtons.style.display = 'block';
+      $editButtons.style.display = 'none';
+      $editInput.value = $label.innerText;
+    }
+  };
+
+  const editTodo = (e) => {
+    if (e.target.className === 'todo_edit_confirm_button' || e.keyCode === 13) {
+      const $item = e.target.closest('.item');
+      const id = $item.dataset.id;
+      const $editInput = $item.querySelector('input[type="text"]');
+      const content = $editInput.value;
+
+      fetch(`${API_URL}/${id}`, {
+        method: 'PATCH',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify({ content }),
+      })
+        .then((response) => response.json())
+        .then(getTodos)
+        .catch((error) => console.error(error.message));
+    }
+  };
 
   const removeTodo = (e) => {
-    if (e.target.className !== 'todo_remove_button') return
-    const $item = e.target.closest('.item')
-    const id = $item.dataset.id
+    if (e.target.className !== 'todo_remove_button') return;
+    const $item = e.target.closest('.item');
+    const id = $item.dataset.id;
 
     fetch(`${API_URL}/${id}`, {
       method: 'DELETE',
     })
       .then((response) => response.json())
       .then(getTodos)
-      .catch((error) => console.error(error.message))
-  }
+      .catch((error) => console.error(error.message));
+  };
 
   const init = () => {
     window.addEventListener('DOMContentLoaded', () => {
-      getTodos()
-      pagination()
-    })
+      getTodos();
+      pagination();
+    });
 
-    $form.addEventListener('submit', addTodo)
-    $todos.addEventListener('click', toggleTodo)
-    $todos.addEventListener('click', changeEditMode)
-    $todos.addEventListener('click', editTodo)
-    $todos.addEventListener('click', removeTodo)
-  }
+    $form.addEventListener('submit', addTodo);
+    $todos.addEventListener('click', toggleTodo);
+    $todos.addEventListener('click', changeEditMode);
+    $todos.addEventListener('keydown', changeEditMode);
+    $todos.addEventListener('click', editTodo);
+    $todos.addEventListener('keydown', editTodo);
+    $todos.addEventListener('click', removeTodo);
+    $todos.addEventListener('click', recommendTodo);
+  };
 
-  init()
-})()
+  init();
+})();
